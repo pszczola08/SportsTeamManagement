1d25c1764aefb9c0213aaeae9ad6cfb4c4dc14f9
pszczola08 <marceliszerszen08@gmail.com>
2024-12-15 17:49:02 +0100

    Final commit

--- Program.cs
+++ Program.cs
@@ -3,5 +3,45 @@
 class Program {
     public static void Main(string[] args) {
         Console.Write("Enter name of your team: ");
+        string? name = Console.ReadLine();
+
+        Team Team = new(name);
+
+        while (true) {
+            Console.Write("\nWhat do you want to do? (A - Add Player, B - Add Game, C - Show statistics, D - Player scored): ");
+            string? c = Console.ReadLine();
+            switch (c) {
+                case "A":
+                    Console.Write("Enter name of the player: ");
+                    string? plname = Console.ReadLine();
+                    Console.Write("Enter position of the player: ");
+                    string? position = Console.ReadLine();
+                    Team.AddPlayer(plname, position);
+                    break;
+                case "B":
+                    Console.Write("Enter name of the game: ");
+                    string? gamename = Console.ReadLine();
+                    Console.Write("Did you win? (0, 1): ");
+                    string? win = Console.ReadLine();
+                    bool boolWin;
+                    if (win == "1") {
+                        boolWin = true;
+                    } else {
+                        boolWin = false;
+                    }
+                    Team.AddGame(gamename, boolWin);
+                    break;
+                case "C":
+                    Console.WriteLine($"Your team wins {Team.Stats()}% of times.");
+                    break;
+                case "D":
+                    Console.Write("Who scored?: ");
+                    string? pl = Console.ReadLine();
+                    Team.Score(pl);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
--- Team.cs
+++ Team.cs
@@ -12,9 +12,22 @@ class Team {
         Games = new();
         PlayerList = new();
     }
+
+    public void AddPlayer(string name, string position) {
+        PlayerList.Add(new Player(name, position));
+    }
+    public void AddGame(string name, bool win) {
+        Games.Add(new(name, win));
+    }
+    public double Stats() {
+        return Games.Count > 0 ? ((double)Games.Where(game => game.Win).Count() / Games.Count) * 100 : 0;
+    }
+    public void Score(string name) {
+        PlayerList.First(player => player.Name == name).PlayerScore();
+    }
 }
 
-record Game {
-    public string Title;
-    public bool Win;
-}
+record Game (
+    string Title,
+    bool Win
+);
